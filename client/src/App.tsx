1a09aed74d95f54635deb369a5530a79c49ee616
B3bq <sebastian.kaca@poczta.onet.pl>
2026-04-10 23:01:15 +0200

    weak up backend from front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Menu from './screens/Menu';
 import Game from './screens/Game';
 import Lobby from './screens/Lobby';
@@ -8,6 +9,41 @@ import Create from "./screens/Create";
 import GameHost from "./screens/GameHost";
 
 function App() {
+  const [backendReady, setBackendReady] = useState(false);
+
+  useEffect(() => {
+    const checkBackend = async () => {
+      try {
+        const response = await fetch("https://multiquiz.onrender.com");
+        if (response.ok) {
+          setBackendReady(true);
+          return true;
+        }
+      } catch (error) {
+        return false;
+      }
+  };
+
+  const interval = setInterval(async () => {
+    const isReady = await checkBackend();
+    if (isReady) clearInterval(interval);
+  }, 3000);
+
+  checkBackend();
+
+  return () => clearInterval(interval);
+  }, []);
+
+  if (!backendReady) {
+    return (
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
+        <div className="spinner"></div>
+        <p>Server is initializing... (it could take a few seconds)</p>
+      </div>
+    );
+  }
+
+
   return(
   <Routes>
       <Route path="/" element={<Menu />} />
